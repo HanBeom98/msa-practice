b3818ca762b3203c62aeb2d8cb2551d2a195ba07
HanBeom98 <Carry9691@gmail.com>
2024-11-21 15:49:00 +0900

    feat : gateway

--- order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
+++ order/src/main/java/com/spring_cloud/eureka/client/order/OrderController.java
@@ -3,7 +3,6 @@ package com.spring_cloud.eureka.client.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +11,8 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @GetMapping("/order/{orderId}")
-    public String getOrder(@PathVariable("orderId") String orderId) {
-        return orderService.getOrder(orderId);
+    @GetMapping("/order/")
+    public String getOrder() {
+        return "Order detail";
     }
 }
